e99fa2fb1992cb46f6a8b089cc9c5e4a7b202c4a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-06 11:46:42 +0530

    display_ui

--- src/Assignment1/Book.java
+++ src/Assignment1/Book.java
@@ -9,8 +9,6 @@ public class Book {
 	String publication;
 	String dateOfPublication;
 	float priceOfBook;
-	int totalQuantityToOrder;
-	float totalCost;
 	
 	Book(){}
 	
@@ -25,5 +23,73 @@ public class Book {
 		this.totalQuantityToOrder = totalQuantityToOrder;
 		this.totalCost = totalCost;
 	}
+	
+	public int getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(int bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public String getAuthorNames() {
+		return authorNames;
+	}
+
+	public void setAuthorNames(String authorNames) {
+		this.authorNames = authorNames;
+	}
+
+	public String getPublication() {
+		return publication;
+	}
+
+	public void setPublication(String publication) {
+		this.publication = publication;
+	}
+
+	public String getDateOfPublication() {
+		return dateOfPublication;
+	}
+
+	public void setDateOfPublication(String dateOfPublication) {
+		this.dateOfPublication = dateOfPublication;
+	}
+
+	public float getPriceOfBook() {
+		return priceOfBook;
+	}
+
+	public void setPriceOfBook(float priceOfBook) {
+		this.priceOfBook = priceOfBook;
+	}
+
+	public int getTotalQuantityToOrder() {
+		return totalQuantityToOrder;
+	}
+
+	public void setTotalQuantityToOrder(int totalQuantityToOrder) {
+		this.totalQuantityToOrder = totalQuantityToOrder;
+	}
+
+	public float getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(float totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	int totalQuantityToOrder;
+	float totalCost;
+	
 	
 }
--- src/Assignment1/Book_Display.java
+++ src/Assignment1/Book_Display.java
@@ -1,14 +1,49 @@
 package Assignment1;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Book_Display {
 
-	public static void main(String[] args) {
-		JFrame j = new JFrame();
-		j.setSize(420,420);
-		j.setVisible(true);
-		
-	}
+    public static void main(String[] args) {
+        // Create a JFrame
+        JFrame j = new JFrame("Book Display");
+        j.setSize(800, 400);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Create a list of books
+        List<Book> books = new ArrayList<>();
+        books.add(new Book(1, "Java Programming", "James Gosling", "Sun Microsystems", "01-01-1995", 500.0f, 10, 5000.0f));
+        books.add(new Book(2, "Effective Java", "Joshua Bloch", "Addison-Wesley", "05-05-2008", 650.0f, 8, 5200.0f));
+        books.add(new Book(3, "Head First Java", "Kathy Sierra, Bert Bates", "O'Reilly Media", "02-06-2005", 450.0f, 15, 6750.0f));
+
+        // Create a table model
+        String[] columnNames = {"Book ID", "Book Name", "Author Names", "Publication", "Date of Publication", "Price of Book", "Total Quantity to Order", "Total Cost"};
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+
+        // Add rows to the table model
+        for (Book book : books) {
+            Object[] rowData = {
+                book.getBookId(),
+                book.getBookName(),
+                book.getAuthorNames(),
+                book.getPublication(),
+                book.getDateOfPublication(),
+                book.getPriceOfBook(),
+                book.getTotalQuantityToOrder(),
+                book.getTotalCost()
+            };
+            tableModel.addRow(rowData);
+        }
+
+        // Create a JTable and set the model
+        JTable table = new JTable(tableModel);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        // Add the scroll pane to the frame
+        j.add(scrollPane);
+        j.setVisible(true);
+    }
 }
