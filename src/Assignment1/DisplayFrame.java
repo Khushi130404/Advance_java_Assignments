5b2fcbefe3f883cd83a36e449757727d96a33fc5
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-01-17 21:52:07 +0530

    Display 4-4 UI

--- src/Assignment1/DisplayFrame.java
+++ src/Assignment1/DisplayFrame.java
@@ -1,71 +1,90 @@
 package Assignment1;
 
-import java.awt.Color;
-import java.awt.GridLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.List;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import java.util.StringTokenizer;
 
-class DisplayFrame extends JFrame
-{
-	List<Book> books;
+public class DisplayFrame extends JFrame {
+    List<Book> books;
     JPanel mainPanel;
     JScrollPane scrollPane;
 
-	public DisplayFrame(List<Book> books) 
-	{
-		this.books = books;
-		this.mainPanel = new JPanel();
-		scrollPane = new JScrollPane(mainPanel);
-		createFrame();
-	}
-	
-	void createFrame()
-	{
-		createMainPanel();
-		createBookPanels();
+    public DisplayFrame(List<Book> books) {
+        this.books = books;
+        this.mainPanel = new JPanel();
+        scrollPane = new JScrollPane(mainPanel);
+        createFrame();
+    }
+
+    void createFrame() {
+        createMainPanel();
+        createBookPanels();
         this.setSize(800, 600);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
         this.add(scrollPane);
         this.setVisible(true);
-	}
-	
-	void createScrollPane()
-	{
+    }
+
+    void createScrollPane() {
         scrollPane.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
-	}
-	
-	void createMainPanel()
-	{
-		 mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-	     mainPanel.setBackground(new Color(230, 230, 250));
-	}
-	
-	void createBookPanels()
-	{
-		for(Book book : books)
-		{
-			JPanel bookPanel = new JPanel();
-            bookPanel.setLayout(new GridLayout(4, 2, 10, 5));
-            bookPanel.setBorder(BorderFactory.createTitledBorder("Book ID: " + book.getBookId()));
+    }
+
+    void createMainPanel() {
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        mainPanel.setBackground(new Color(230, 230, 250));
+    }
+
+    void createBookPanels() {
+        for (Book book : books) {
+            JPanel bookPanel = new JPanel(new BorderLayout(10, 5));
             bookPanel.setBackground(Color.WHITE);
             
-            bookPanel.add(new JLabel("Book Name: " + book.getBookName()));
-            bookPanel.add(new JLabel("Author: " + book.getAuthorNames()));
-            bookPanel.add(new JLabel("Publication: " + book.getPublication()));
-            bookPanel.add(new JLabel("Date: " + book.getDateOfPublication()));
-            bookPanel.add(new JLabel("Price: " + book.getPriceOfBook()));
-            bookPanel.add(new JLabel("Quantity: " + book.getTotalQuantityToOrder()));
-            bookPanel.add(new JLabel("Total Cost: " + book.getTotalCost()));
+            JPanel detailsPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+            JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 5));
+            
+            detailsPanel.add(new JLabel("Book ID: " + book.getBookId()));
+            detailsPanel.add(new JLabel("Book Name: " + book.getBookName()));
+            detailsPanel.add(new JLabel("Author: " + book.getAuthorNames()));
+            detailsPanel.add(new JLabel("Publication: " + book.getPublication()));
+            detailsPanel.add(new JLabel("Date: " + book.getDateOfPublication()));
+            detailsPanel.add(new JLabel("Price: " + book.getPriceOfBook()));
+            detailsPanel.add(new JLabel("Quantity: " + book.getTotalQuantityToOrder()));
+            detailsPanel.add(new JLabel("Total Cost: " + book.getTotalCost()));
+            
+            JButton updateButton = new JButton("Update");
+            JButton deleteButton = new JButton("Delete");
+            
+            updateButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(null, "Update button clicked for Book ID: " + book.getBookId());
+                }
+            });
+            
+            deleteButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    mainPanel.remove(bookPanel);
+                    mainPanel.revalidate();
+                    mainPanel.repaint();
+                }
+            });
+            
+            buttonPanel.add(updateButton);
+            buttonPanel.add(deleteButton);
+            
+            bookPanel.add(detailsPanel, BorderLayout.CENTER);
+            bookPanel.add(buttonPanel, BorderLayout.SOUTH);
             
             mainPanel.add(bookPanel);
             mainPanel.add(Box.createVerticalStrut(10));
-		}
-	}
+        }
+    }
 }
