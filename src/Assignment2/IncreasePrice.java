105bde07a67e6c786d45e3dfbac40c8964e8f74a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-04 09:20:13 +0530

    Increase Price of Books

---
+++ src/Assignment2/IncreasePrice.java
@@ -0,0 +1,38 @@
+package Assignment2;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.swing.JOptionPane;
+
+public class IncreasePrice 
+{
+
+    Connection con;
+	CallableStatement cst;
+	
+	public IncreasePrice() 
+	{
+		increasePriceOfBooks();
+	}
+	
+	void increasePriceOfBooks()
+	{
+        try 
+        {	
+        	Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/books?characterEncoding=latin1", "root", "khushi");
+			cst = con.prepareCall("{ call update_book_prices() }");
+            cst.execute();
+            cst.close();
+            con.close();
+		}
+        catch (Exception e) 
+        {
+			e.printStackTrace();
+		}
+
+	}
+
+}
