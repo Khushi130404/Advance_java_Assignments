89941fa13ba7420dd074a39a00692730d177ba8a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-02-14 12:37:56 +0530

    UDPFileClient

--- src/Assignment3/Question2/UDPFileServer.java
+++ src/Assignment3/Question2/UDPFileServer.java
@@ -5,59 +5,67 @@ import java.io.File;
 import java.io.FileReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.Socket;
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 public class UDPFileServer 
 {
-	public static void main(String[] args) 
-	{
-		try
-		{
-			DatagramSocket server = new DatagramSocket(2001);
-			InetAddress ip = InetAddress.getByName("localhost");
-			DatagramPacket packet;
-	        String filePath = "";
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            DatagramSocket server = new DatagramSocket(2001);
+            System.out.println("Server is running...");
 
-	        while(!filePath.equalsIgnoreCase("Quit"))
-	        {
-	        	// Get File
-				byte[] filePathByte = new byte[1024];
-				packet = new DatagramPacket(filePathByte,filePathByte.length,ip,1304);
-	        	server.receive(packet);
-				filePathByte = packet.getData();
-		        filePath = new String(filePathByte);
-				
-				if(filePath.equalsIgnoreCase("Quit")) break;
-		        System.out.println(filePath+"...");
-		        File f=new File(filePath);
-		        if (f.isFile())
-		        {
-			        // Read & Send File
-		            System.out.println("File found");
-		            BufferedReader reader = new BufferedReader(new FileReader(f));
-			        String line;
-			        while ((line = reader.readLine()) != null) 
-			        {
-						byte[] fileContentByte = new byte[1024];
-			        	packet = new DatagramPacket(fileContentByte,fileContentByte.length,ip,1304);
-			            server.send(packet);
-			        }
-			        String eof = "EOF";
-			    	byte[] eofByte = new byte[1024];
-			        packet = new DatagramPacket(eofByte,eofByte.length,ip,1304);
-			        server.send(packet);
-		        }
-		        else
-		        {
-		        	System.out.println("File Not Found");
-		        }
-	        }
-	        server.close();
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
-	}
+            DatagramPacket packet;
+            String filePath = "";
+
+            while (true) 
+            {
+                // Receive File Path
+                byte[] filePathByte = new byte[1024];
+                packet = new DatagramPacket(filePathByte, filePathByte.length);
+                server.receive(packet);
+
+                filePath = new String(packet.getData(), 0, packet.getLength()).trim();
+                if (filePath.equalsIgnoreCase("Quit")) break;
+
+                System.out.println("Requested file: " + filePath);
+
+                File f = new File(filePath);
+                if (f.isFile()) 
+                {
+                    System.out.println("File found. Sending...");
+                    BufferedReader reader = new BufferedReader(new FileReader(f));
+                    String line;
+                    while ((line = reader.readLine()) != null) 
+                    {
+                        byte[] fileContentByte = line.getBytes();
+                        packet = new DatagramPacket(fileContentByte, fileContentByte.length, 
+                                packet.getAddress(), packet.getPort());
+                        server.send(packet);
+                    }
+                    reader.close();
+
+                    // Send EOF signal
+                    String eof = "EOF";
+                    byte[] eofByte = eof.getBytes();
+                    packet = new DatagramPacket(eofByte, eofByte.length, packet.getAddress(), packet.getPort());
+                    server.send(packet);
+                } 
+                else 
+                {
+                    System.out.println("File Not Found");
+                    String notFound  = "File not Found...!";
+                    byte[] notFoundByte = notFound.getBytes();
+                    packet = new DatagramPacket(notFoundByte, notFoundByte.length, packet.getAddress(), packet.getPort());
+                    server.send(packet);
+                }
+            }
+            System.out.println("Server shutting down...");
+            server.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
