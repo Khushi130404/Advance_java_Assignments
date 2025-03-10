bee75940494afc744369246116d6b7445336e3f8
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-03-10 23:09:21 +0530

    Proxy Server

--- src/Assignment3/Question6/ProxyServer.java
+++ src/Assignment3/Question6/ProxyServer.java
@@ -1,5 +1,53 @@
 package Assignment3.Question6;
 
-public class ProxyServer {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.*;
 
+class ProxyServer 
+{
+    public static void main(String args[]) 
+    {
+        ServerSocket serverSocket = null;
+        Socket server = null;
+        Socket client = null;
+        DataInputStream serverInputStream = null;
+        DataOutputStream serverOutputStream = null;
+        HandleClientRequest handleClientRequest = null;
+        try 
+        {
+            serverSocket = new ServerSocket(8081);
+            server = new Socket("localhost", 8082);
+            serverInputStream = new DataInputStream(server.getInputStream());
+            serverOutputStream = new DataOutputStream(server.getOutputStream());
+            handleClientRequest = new HandleClientRequest(serverInputStream, serverOutputStream);
+            while(true) 
+            {
+                client = serverSocket.accept();
+                ClientRequest newClient = new ClientRequest(client, handleClientRequest);
+                newClient.start();
+                System.out.println("New Client Connected ...");
+            }
+
+        } 
+        catch(Exception e) 
+        {
+            System.out.println("Error on Proxy Server ...");
+            System.out.println(e);
+        } 
+        finally 
+        {
+            try 
+            {
+                serverOutputStream.close();
+                serverInputStream.close();
+                server.close();
+                serverSocket.close();
+            } 
+            catch(Exception e) 
+            {
+            	e.printStackTrace();
+            }
+        }
+    }
 }
